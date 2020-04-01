fff0ab553e59d4c7c16bec9a0b650fe14e07036a
luke <2733624354@qq.com>
2020-04-01 16:29:03 +0800

    Add random number part into my extension, this part need GNU-GSL library.

---
+++ source/RNG.h
@@ -0,0 +1,130 @@
+//
+// Created by luke on 2020/4/1.
+//
+
+#ifndef RNG_H
+#define RNG_H
+
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_combination.h>
+#include <cstdio>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+class RNG
+{
+    const gsl_rng_type *GRT;
+    gsl_rng *gr;
+    long int initial_seed;
+    long int _seed;
+
+public:
+
+    RNG(long int seed = 0)
+    {
+        gsl_rng_env_setup();
+        GRT = gsl_rng_default;
+        gr = gsl_rng_alloc(GRT);
+        gsl_rng_set(gr, seed);
+        _seed = seed;
+        initial_seed = seed;
+    }
+
+    ~RNG()
+    {
+        gsl_rng_free(gr);
+    }
+
+    /*!
+     * 生成[low,high]的随机整数
+     * @param low
+     * @param high
+     * @return
+     */
+    unsigned long Randint(int low, int high){
+        return (unsigned long) (gsl_rng_uniform(gr) * (high - low) + low
+            + 0.5); // +0.5 para que al truncar tanto low como high est��n incluidos
+    }
+
+    /*!
+     * range [low,high] floating
+     * @param low
+     * @param high
+     * @return
+     */
+    double Randfloat(double low, double high){
+        return (gsl_rng_uniform(gr) * (high - low) + low);
+    }
+
+    /*!
+     * 对数组进行洗牌
+     * @param perm
+     */
+    template<typename T>
+    void RandPerm(std::vector<T> &perm)
+    {
+        for (int i = 0; i < perm.size(); i++) {
+            int change = this->Randint(0, perm.size() - 1);
+            swap(perm[i],perm[change]);
+        }
+    }
+
+    void change(long int seed){
+        gsl_rng_set(gr, seed);
+        _seed = seed;
+    }
+
+    void inline show_seed()
+    {
+        printf("random seed is : %ld\n", _seed);
+    }
+
+    inline void restore()
+    {
+        gsl_rng_set(gr, initial_seed);
+        _seed = initial_seed;
+    }
+};
+
+
+class COMB
+{
+    gsl_combination *comb;
+    vector<int> combinations;
+    bool available;
+public:
+    COMB(const int c, const int r)
+    {
+        assert(c >= r);
+        comb = gsl_combination_calloc(c, r);
+        combinations.reserve(r);
+        combinations.clear();
+        available = true;
+    }
+
+    ~COMB()
+    {
+        gsl_combination_free(comb);
+    };
+
+    bool get_combinations(vector<int> &buffer)
+    {
+        if (available) {
+            combinations.clear();
+            for (auto i = 0; i < gsl_combination_k(comb); i++) {
+                combinations.push_back(gsl_combination_get(comb, i));
+            }
+            buffer = combinations;
+            available = (gsl_combination_next(comb) == GSL_SUCCESS);
+            return true;
+        }
+        else
+            return false;
+    }
+
+};
+
+
+#endif //RNG_H
--- source/wrapper.cpp
+++ source/wrapper.cpp
@@ -3,13 +3,15 @@
 
 #include "source.h"
 #include "file.h"
+#include "RNG.h"
 
 namespace py = pybind11;
 
 
 //templeate of ordinary funtion
 PYBIND11_MODULE(McgrpUtils, m) {
-    m.doc() = "Interface of mcgrp"; // optional module docstring
+    m.doc() = "A dedicated library of heuristic algos for"
+              "Mixed Capacitated General Routing Problem.";
 
     //constants
     m.attr("DUMMY") = DUMMY;
@@ -24,10 +26,22 @@ PYBIND11_MODULE(McgrpUtils, m) {
         .def_readwrite("req_arc_num", &instance_num_information::req_arc_num)
         .def_readwrite("req_node_num", &instance_num_information::req_node_num);
 
-    //fumctions
-    m.def("get_negative_coding",&get_negative_coding,"get negative coding format");
-    m.def("get_delimeter_coding", &get_delimeter_coding,"get zero coding format");
 
-    m.def("GetTasksNum",&GetTasksNum,"get instance meta info");
+    //classes
+    //I decided not to expose template method cause I think I dont need to call it in python
+    //If you want to use it, you need to specialization it :)
+    py::class_<RNG>(m,"RNG")
+        .def(pybind11::init<int>(), py::arg("seed") = 0)
+        .def("Randint",&RNG::Randint, py::arg("low"),py::arg(("high")))
+        .def("Randfloat",&RNG::Randfloat, py::arg("low"),py::arg("high"))
+        .def("change", &RNG::change, py::arg("seed"))
+        .def("show_seed",&RNG::show_seed)
+        .def("restore", &RNG::restore);
+
+    //fumctions
+    m.def("get_negative_coding",&get_negative_coding,py::arg("zero format seq"));
+    m.def("get_delimeter_coding", &get_delimeter_coding,py::arg("negative format seq"));
+
+    m.def("GetTasksNum",&GetTasksNum,py::arg("filename"),py::arg("instance metainfo ptr"));
 }
 
